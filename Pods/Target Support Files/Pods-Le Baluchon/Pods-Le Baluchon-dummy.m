3cfe679005610a8203c38ad9d0b6954f584a3224
Simon Darrieumerlou <simon-64@hotmail.com>
2019-09-19 13:26:03 +0200

    Removing the framework SwiftGooglTranslate

--- Pods/Target Support Files/Pods-Le Baluchon/Pods-Le Baluchon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Le_Baluchon : NSObject
-@end
-@implementation PodsDummy_Pods_Le_Baluchon
-@end
--- Pods/Target Support Files/SwiftGoogleTranslate/SwiftGoogleTranslate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftGoogleTranslate : NSObject
-@end
-@implementation PodsDummy_SwiftGoogleTranslate
-@end
